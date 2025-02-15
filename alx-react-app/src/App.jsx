6662972a43d4c77d71372a0acece95999db89947
OgborFaith <faithogbor23@gmail.com>
2025-02-15 07:58:59 +0100

    updated the changes in the react app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,10 @@
+import WelcomeMessage from "./components/WelcomeMessage";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -28,8 +27,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <WelcomeMessage />
     </>
   )
 }
-
-export default App
+export default App;
