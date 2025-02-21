6c101f177065a2aca96788b27abef6893a6cc5b7
OgborFaith <faithogbor23@gmail.com>
2025-02-21 07:58:16 +0100

    Added Counter component to the directory

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from "./components/UserProfile";
+import Counter from './components/Counter';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -36,6 +37,7 @@ function App() {
       <Header />
        <MainContent />
        <Footer />
+       <Counter/>
     </>
   )
 }
