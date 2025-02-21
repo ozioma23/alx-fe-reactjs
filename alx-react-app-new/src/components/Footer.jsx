a677a7021cfb77e38ac42f83d4976d696274f8d4
OgborFaith <faithogbor23@gmail.com>
2025-02-21 07:23:35 +0100

    new React-app added to the repo

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+        <footer>
+  <p style={{ backgroundColor: 'white',fontStyle: 'italics'}}>© 2023 City Lovers</p>
+</footer>
+    )
+}
+export default Footer
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent () {
+    return (
+        <main style={{ backgroundColor: 'blue', color: 'grey', border: '1px solid white',}}>
+        <p style= {{fontWeight: 'bold'}}>I love to visit New York, Paris, and Tokyo.</p>
+      </main> 
+    )
+}
+export default MainContent
