998e94df0c95b205ccc0a1e64d76b2d417c56342
OgborFaith <faithogbor23@gmail.com>
2025-02-28 07:47:38 +0100

    changes made to the recipe app

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,6 +1,5 @@
 import {create} from 'zustand'
-
-const useRecipeStore = create(set => ({
+ const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes })
