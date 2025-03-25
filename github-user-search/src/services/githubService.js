86814672872464f6437609999691ff613a88b9d1
OgborFaith <faithogbor23@gmail.com>
2025-03-25 20:59:10 +0100

    add the changes made

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -14,7 +14,7 @@ const fetchUserData = async (username, location = "", minRepos = "") => {
             query += `+repos:>${minRepos}`;
         }
 
-        const response = await axios.get(`https://api.github.com/search/users?${query}`, {
+        const response = await axios.get(`https://api.github.com/search/users?q${query}`, {
             headers: {
                 Authorization: `token ${API_KEY}`, // Pass API key if required
                 Accept: "application/vnd.github.v3+json",
